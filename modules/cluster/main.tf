632ba444aadc55959a73a6537f83d0aeae395323
Justin Schwartzbeck <justinmschw@gmail.com>
2026-01-23 23:16:04 -0600

    fix tf definition

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -34,26 +34,24 @@ module "master" {
   username = var.username
 }
 
-dynamic "worker" {
+module "worker_pool" {
   for_each = { for idx, id in var.vm_ids : idx => id if idx != 0 }
-  content {
-    source = "../appliance/server"
-    proxmox_endpoint = var.proxmox_endpoint
-    proxmox_api_user = var.proxmox_api_user
-    proxmox_api_token = var.proxmox_api_token
-    proxmox_node_name = var.proxmox_node_name
-    proxmox_user = var.proxmox_user
-    proxmox_password = var.proxmox_password
-    server_name = "${var.cluster_name}-worker-${worker.key}"
-    vm_id = worker.value
-    size = "small"
-    datastore_id = var.datastore_id
-    ssh_public_keys = var.ssh_public_keys
-    networks = length(var.networks) > 0 ? var.networks : [ { bridge = "vmbr0", address = "dhcp" } ]
-    source_vm_id = var.source_vm_id
-    source_vm_datastore = var.source_vm_datastore
-    username = var.username
-  }
+  source = "../appliance/server"
+  proxmox_endpoint = var.proxmox_endpoint
+  proxmox_api_user = var.proxmox_api_user
+  proxmox_api_token = var.proxmox_api_token
+  proxmox_node_name = var.proxmox_node_name
+  proxmox_user = var.proxmox_user
+  proxmox_password = var.proxmox_password
+  server_name = "${var.cluster_name}-worker-${worker.key}"
+  vm_id = worker.value
+  size = "small"
+  datastore_id = var.datastore_id
+  ssh_public_keys = var.ssh_public_keys
+  networks = length(var.networks) > 0 ? var.networks : [ { bridge = "vmbr0", address = "dhcp" } ]
+  source_vm_id = var.source_vm_id
+  source_vm_datastore = var.source_vm_datastore
+  username = var.username
 }
 
 resource "bitwarden_folder" "clusterinfo" {
