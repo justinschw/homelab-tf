621f83e7b216a4c0d76e54a98182734e435eb0b0
Justin Schwartzbeck <justinmschw@gmail.com>
2026-01-23 22:57:23 -0600

    adds cluster module

---
+++ modules/cluster/main.tf
@@ -0,0 +1,74 @@
+terraform {
+  backend "s3" {}
+  required_providers {
+    bitwarden = {
+      source = "bitwarden/bitwarden"
+      version = ">= 1.17.0"
+    }
+  }
+}
+
+provider "bitwarden" {
+  email            = var.bitwarden_email
+  master_password  = var.bitwarden_password
+  client_id        = var.bitwarden_client_id
+  client_secret    = var.bitwarden_client_secret
+}
+
+module "master" {
+  source = "../appliance/server"
+  proxmox_endpoint = var.proxmox_endpoint
+  proxmox_api_user = var.proxmox_api_user
+  proxmox_api_token = var.proxmox_api_token
+  proxmox_node_name = var.proxmox_node_name
+  proxmox_user = var.proxmox_user
+  proxmox_password = var.proxmox_password
+  server_name = "${var.cluster_name}-master"
+  vm_id = var.vm_ids[0]
+  size = "medium"
+  datastore_id = var.datastore_id
+  ssh_public_keys = var.ssh_public_keys
+  networks = length(var.networks) > 0 ? var.networks : [ { bridge = "vmbr0", address = "dhcp" } ]
+  source_vm_id = var.source_vm_id
+  source_vm_datastore = var.source_vm_datastore
+  username = var.username
+}
+
+dynamic "worker" {
+  for_each = { for idx, id in var.vm_ids : idx => id if idx != 0 }
+  content {
+    source = "../appliance/server"
+    proxmox_endpoint = var.proxmox_endpoint
+    proxmox_api_user = var.proxmox_api_user
+    proxmox_api_token = var.proxmox_api_token
+    proxmox_node_name = var.proxmox_node_name
+    proxmox_user = var.proxmox_user
+    proxmox_password = var.proxmox_password
+    server_name = "${var.cluster_name}-worker-${worker.key}"
+    vm_id = worker.value
+    size = "small"
+    datastore_id = var.datastore_id
+    ssh_public_keys = var.ssh_public_keys
+    networks = length(var.networks) > 0 ? var.networks : [ { bridge = "vmbr0", address = "dhcp" } ]
+    source_vm_id = var.source_vm_id
+    source_vm_datastore = var.source_vm_datastore
+    username = var.username
+  }
+}
+
+resource "bitwarden_folder" "clusterinfo" {
+  name = "${var.cluster_name}-info"
+}
+
+resource "bitwarden_item_secure_note" "ansible_inventory" {
+  folder_id = bitwarden_folder.clusterinfo.id
+  name      = "${var.cluster_name}-ansible-inventory"
+  notes     = << EOT
+  [masters]
+  ${module.master.server_name} ansible_host=${module.master.ip_address} ansible_user=${var.username}
+  [workers]
+  %{ for w in worker.value ~}
+  ${w.server_name} ansible_host=${w.ip_address} ansible_user=${var.username}
+  %{ endfor ~}
+  EOT
+}
--- modules/cluster/variables.tf
+++ modules/cluster/variables.tf
@@ -1,4 +1,4 @@
-# Provider auth
+# Proxmox info
 variable "proxmox_endpoint" {
     type = string
 }
@@ -27,23 +27,31 @@ variable "proxmox_password" {
     sensitive = true
 }
 
+# Bitwarden info
+
+variable "bitwarden_email" {
+    type = string
+}
+
+variable "bitwarden_password" {
+    type = string
+    sensitive = true
+}
+
+variable "bitwarden_client_id" {
+    type = string
+    sensitive = true
+}
+
+variable "bitwarden_client_secret" {
+    type = string
+    sensitive = true
+}
+
 # VM info
 
-variable "server_name" {
+variable "datastore_id" {
     type = string
-}
-
-variable "vm_id" {
-    type = number
-    default = 100
-}
-
-variable "size" {
-    type = string
-    validation {
-        condition = can(regex("^(small|medium|large)$", var.size))
-        error_message = "Must be small, medium or large"
-    }
 }
 
 variable "ssh_public_keys" {
@@ -58,39 +66,33 @@ variable "networks" {
         address = string
         gateway = optional(string)
     }))
-    default = [
-        {
-            bridge = "vmbr0"
-            address = "dhcp"
-        }
-    ]
-}
-
-variable "datastore_id" {
-    type = string
-    default = "vm-tank"
-}
-
-# Optional data disk
-variable "data_disk_size_gb" {
-    type = number
-    default = 0
-}
-
-# User info
-variable "username" {
-    type = string
-    default = "homelab"
-}
-
-variable "source_vm_datastore" {
-    description = "Datastore of the source VM to clone from"
-    type = string
-    default = "vm-tank"
+    default = []
 }
 
 variable "source_vm_id" {
     description = "When non-zero, clone from this source VM ID"
     type = number
     default = 0
+}
+
+variable "source_vm_datastore" {
+    description = "Datastore of the source VM to clone from"
+    type = string
+    default = ""
+}
+
+variable "username" {
+    type = string
+    default = "k3s"
+}
+
+# Cluster info
+
+variable "cluster_name" {
+    type = string
+}
+
+variable "vm_ids" {
+    type = list(number)
+    default = []
 }
