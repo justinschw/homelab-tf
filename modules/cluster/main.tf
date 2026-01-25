63e9fc78521bb3d828c24c334c6d4283cbdd3721
Justin Schwartzbeck <justinmschw@gmail.com>
2026-01-25 12:22:04 -0600

    mac_address

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -127,7 +127,7 @@ resource "pfsense_dhcpv4_staticmapping" "dhcp_mappings" {
     vm.server_name => vm
   }
   interface         = "lan"
-  mac               = each.value.mac
+  mac_address       = each.value.mac
   ip_address        = data.dns_a_record_set.vm_ips[each.key].addresses[0]
   hostname          = each.value.hostname
   description       = "Static DHCP mapping for ${each.value.hostname}"
